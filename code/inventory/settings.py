31aecc10f79656ce2884a3c392b3c38a47f593b3
vdvelde <vdvelde.t@gmail.com>
2019-12-09 21:09:25 +0100

    curl test is too fast giving errors

--- code/inventory/settings.py
+++ code/inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['192.168.0.5']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
