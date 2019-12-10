df20dbadce2b94feabc29d26ab2ebd641c2febec
vdvelde <vdvelde.t@gmail.com>
2019-12-10 18:32:21 +0100

    More loggin on travis loading

--- code/populate_database.py
+++ code/populate_database.py
@@ -3,7 +3,14 @@
 
 import os
 import sys
+import logging
+
+logger = logging.getLogger('Populate database')
+self.log = logger
+self.log.info("START : loading data")
 
 from api import tests
 data = tests.InventoryCase()
 data.setUp()
+
+self.log.info("END : loading data")
--- scripts/inventory.py
+++ scripts/inventory.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import urllib.request
-customer = "Jedi"
+tenant = "Jedi"
 django_inventory_url = "http://127.0.0.1:8000/api/inventory/byname"
-contents = urllib.request.urlopen("%s/%s/" % (django_inventory_url, customer)).read()
+contents = urllib.request.urlopen("%s/%s/" % (django_inventory_url, tenant)).read()
 print(contents.decode('utf-8'))
