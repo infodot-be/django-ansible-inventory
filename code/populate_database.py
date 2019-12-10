3685bc346c76237ee03f42eef07b8375621f040d
vdvelde <vdvelde.t@gmail.com>
2019-12-10 18:46:15 +0100

    Error in site population for testing

--- code/populate_database.py
+++ code/populate_database.py
@@ -6,11 +6,11 @@ import sys
 import logging
 
 logger = logging.getLogger('Populate database')
-self.log = logger
-self.log.info("START : loading data")
+log = logger
+log.info("START : loading data")
 
 from api import tests
 data = tests.InventoryCase()
 data.setUp()
 
-self.log.info("END : loading data")
+log.info("END : loading data")
