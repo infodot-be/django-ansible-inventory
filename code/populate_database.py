f0d5c53c582124f88893eee18cfa8bb6922757e8
vdvelde <vdvelde.t@gmail.com>
2019-12-10 18:58:03 +0100

    Shell is not workign on travis

--- code/populate_database.py
+++ code/populate_database.py
@@ -4,12 +4,24 @@
 import os
 import sys
 import logging
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','inventory.settings')
+# Import settings
+django.setup()
+
+from django.conf import settings
+from django.db import IntegrityError
+from api import tests
+
 
 logger = logging.getLogger('Populate database')
 log = logger
 log.info("START : loading data")
 
-from api import tests
+
+
+
 data = tests.InventoryCase()
 data.setUp()
 
